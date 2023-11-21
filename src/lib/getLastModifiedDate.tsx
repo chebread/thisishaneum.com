4dda9d27adf10baa4d5fd3363913177ec723c168
Cha Haneum <developer_haneum_11@icloud.com>
2023-11-21 19:25:37 +0900

    fix: get last modified date error

--- src/lib/getLastModifiedDate.tsx
+++ src/lib/getLastModifiedDate.tsx
@@ -1,9 +1,9 @@
 import fs from 'fs/promises';
 
+// (0): Vercel 배포시 파일 시간이 접근이 안됨. 해결하기
+
 function getLastModifiedDate(filepath: string) {
   const mtime = fs.stat(filepath).then(data => {
-    console.log(data);
-
     return data.mtime;
   });
 
