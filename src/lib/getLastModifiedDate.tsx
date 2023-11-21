afe50f2eb6c3e1de4e477c43630802fce156ad6e
Cha Haneum <developer_haneum_11@icloud.com>
2023-11-21 19:21:01 +0900

    test

--- src/lib/getLastModifiedDate.tsx
+++ src/lib/getLastModifiedDate.tsx
@@ -1,7 +1,11 @@
 import fs from 'fs/promises';
 
 function getLastModifiedDate(filepath: string) {
-  const mtime = fs.stat(filepath).then(data => data.mtime);
+  const mtime = fs.stat(filepath).then(data => {
+    console.log(data);
+
+    return data.mtime;
+  });
 
   // const stats = fs.statSync(filepath).mtime;
 
