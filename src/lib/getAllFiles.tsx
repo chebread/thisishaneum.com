d6b49dba3aa466a7ca0cab658dbf758b76f66697
Cha Haneum <developer_haneum_11@icloud.com>
2023-12-31 23:43:22 +0900

    post: Add, edit, remove posts feat: 디렉토리로 블로그 글 분류할 수 있는 기능 추가함함

---
+++ src/lib/getAllFiles.tsx
@@ -0,0 +1,11 @@
+import fs from 'fs';
+import path from 'path';
+
+const getAllFiles = (dir: any) =>
+  fs.readdirSync(dir).reduce((files, file) => {
+    const name = path.join(dir, file);
+    const isDirectory = fs.statSync(name).isDirectory();
+    return isDirectory ? [...files, ...getAllFiles(name)] : [...files, name];
+  }, []);
+
+export default getAllFiles;
