2e2ccd33316b44803dfef2fb95e3ed89007eeb14
Cha Haneum <developer_haneum_11@icloud.com>
2023-12-31 23:52:26 +0900

    bug: 디렉토리 분립 기능 버그 발생 post: 디렉토리 분립 기능 버그 발생으로 모든 글 posts 폴더로 옮김

--- src/lib/getAllFiles.tsx
+++ src/lib/getAllFiles.tsx
@@ -1,6 +1,8 @@
 import fs from 'fs';
 import path from 'path';
 
+// (0): vercel에서 오류 발생함 해결하기
+
 const getAllFiles = (dir: any) =>
   fs.readdirSync(dir).reduce((files, file) => {
     const name = path.join(dir, file);
--- src/lib/getPosts.tsx
+++ src/lib/getPosts.tsx
@@ -2,12 +2,12 @@ import matter from 'gray-matter';
 import path from 'path';
 import fs from 'fs/promises';
 import { cache } from 'react';
-import getAllFiles from './getAllFiles';
+// import getAllFiles from './getAllFiles';
 // import getLastModifiedDate from './getLastModifiedDate'; // Vercel 서버 사용하므로 이는 불가능한 기능임
 
 const getPosts = cache(async () => {
-  // const posts = await fs.readdir(path.join(process.cwd(), './posts')); // await fs.readdir('./posts/');
-  const posts = getAllFiles('./posts');
+  const posts = await fs.readdir(path.join(process.cwd(), './posts')); // await fs.readdir('./posts/');
+  // const posts = getAllFiles('./posts');
 
   const postsWithMetadata = await Promise.all(
     posts
@@ -15,19 +15,19 @@ const getPosts = cache(async () => {
         file => path.extname(file) === '.md' || path.extname(file) === '.mdx'
       )
       .map(async file => {
-        // const filePath = `./posts/${file}`;
-        const filePath = `./${file}`;
+        const filePath = `./posts/${file}`;
+        // const filePath = `./${file}`;
         const postContent = await fs.readFile(filePath, 'utf8');
         const { data, content } = matter(postContent);
 
         if (data.published === false) {
           return null;
         }
-        // const slug: string = file.substring(0, file.indexOf('.'));
-        const slug: string = file.substring(
-          file.lastIndexOf('/') + 1,
-          file.indexOf('.')
-        ); // file name is blog's slug
+        const slug: string = file.substring(0, file.indexOf('.'));
+        // const slug: string = file.substring(
+        //   file.lastIndexOf('/') + 1,
+        //   file.indexOf('.')
+        // ); // file name is blog's slug
         // 뒤에서 부터 앞에 '/' 까지
 
         // const date = await getLastModifiedDate(filePath);
