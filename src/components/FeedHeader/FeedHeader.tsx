40d8908ab23d2e18bb30b0652b9fe28586bf48b5
Cha Haneum <developer_haneum_11@icloud.com>
2024-03-02 02:43:59 +0900

    feat: remove shadow around feed image

--- src/components/FeedHeader/FeedHeader.tsx
+++ src/components/FeedHeader/FeedHeader.tsx
@@ -49,10 +49,10 @@ const Img = styled(Link)`
   background-size: cover;
 
   border-radius: 1rem;
-  box-shadow: 0 10.5px 21px rgba(0, 0, 0, 0.08);
+  /* box-shadow: 0 10.5px 21px rgba(0, 0, 0, 0.08);
   @media (prefers-color-scheme: dark) {
     box-shadow: 0 10.5px 21px rgba(255, 255, 255, 0.08);
-  }
+  } */
 
   box-sizing: border-box;
 `;
